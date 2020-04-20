71406e8d0fbeb6aa58dfeaaea852c9be3dfddf4d
Angala <47700775+aglpy@users.noreply.github.com>
2020-04-20 17:10:52 +0200

    Add coments and some changes more

--- bot.py
+++ bot.py
@@ -1,17 +1,41 @@
-﻿import telebot
+﻿# -*- coding: utf-8 -*-
+# Codigo para MyBot
+import telebot
 from telebot import types
+from importlib import reload
 
-from m_token import bot
+#Importamos el bot definido por el token en m_token 
+from m_token import bot, mi_id
 
+#Importamos el modulo listener
 from m_listener import listener, reg_arch
 
+#Importamos los modulos de los comandos
 from comandos.m_kyu import command_kyu
 from comandos.m_almuerzo import command_almuerzo
+from comandos.m_batido import command_batido
 
-mi_id = ##
 
+#Asociamos la funcion listener a la propiedad que escucha los mensajes
 bot.set_update_listener(listener)		
 
+#Actualiza los modulos
+@bot.message_handler(commands=["update"])
+def command_update(m):	
+	cid = m.chat.id
+	if cid != mi_id:
+		return 
+	#Crear un actualizador de modulos con reload, no funciona con import . from .
+	
+#Cierra el bot	
+@bot.message_handler(commands=["exit"])
+def command_exit(m):	
+	cid = m.chat.id
+	if cid != mi_id:
+		return 
+	bot.polling(none_stop=False)
+
+#Manda el registro
 @bot.message_handler(commands=["registro"])		
 def command_registro(m):	
 	cid = m.chat.id
@@ -19,6 +43,7 @@ def command_registro(m):
 		return 
 	bot.send_document(cid,open(reg_arch,"rb"))
 
+#Manda una peticion de soporte
 @bot.message_handler(commands=["soporte"])		
 def command_soporte(m):	
 	cid = m.chat.id
@@ -26,8 +51,9 @@ def command_soporte(m):
 		bot.send_message(cid, "Para contactar con el creador usa '/soporte tu mensaje'")
 	else:
 		bot.send_message(mi_id, str(cid) + "," + m.chat.first_name + ":" + m.text)
-		bot.send_message(cid, "Gracias!")
+		bot.send_message(cid, "Thanks!")
 
+#Responde a la peticion de soporte
 @bot.message_handler(commands=["rsoporte"])		
 def command_rsoporte(m):	
 	cid = m.chat.id
@@ -35,5 +61,6 @@ def command_rsoporte(m):
 		return 
 	mt = m.text.split()
 	bot.send_message(mt[1]," ".join(mt[2:]))
-
+	
+#Hacemos que el bot nunca se pare
 bot.polling(none_stop=True)

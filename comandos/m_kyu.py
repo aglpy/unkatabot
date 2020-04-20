90b48323bcbc34d7ffdd9e9d80c42b2779f9c38e
Angala <47700775+aglpy@users.noreply.github.com>
2020-04-20 17:46:43 +0200

    Fixed kyu html reference

--- comandos/m_kyu.py
+++ comandos/m_kyu.py
@@ -15,7 +15,7 @@ def command_kyu(m):
 		mensaje = ""
 		for i in m.text.split()[1:]:	
 			body = BeautifulSoup(leerweb(f"{codewars}{search}{level_ref}{i}{opts}"),"lxml")
-			cats = [cat.find("a")["href"] for cat in body.find_all(attrs={"class":"pvx"})]
+			cats = [cat.find("a")["href"] for cat in body.find_all(attrs={"class":"py-5px"})] # all categories
 			body = BeautifulSoup(leerweb(f"{codewars}{search}{choice(cats)}"),"lxml")
 			katas = [kata.find("a")["href"] for kata in body.find_all(attrs={"class":"list-item kata"})]
 			mensaje += f"{i}kyu: {codewars}{choice(katas)}\n\n"
